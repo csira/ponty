373daea946b0968f9c5292b36bdbb5266004a9ee
chris sira <cbsira@gmail.com>
2022-07-10 10:19:58 -0400

    [render] trap PontyErrors again

--- ponty/http/render.py
+++ ponty/http/render.py
@@ -2,10 +2,12 @@ import functools
 
 import aiohttp.web
 
+from ponty.errors import error_trap
 from ponty.utils import now_millis
 
 
 def render(f):
+    @error_trap
     @functools.wraps(f)
     async def wrapper(*a, **kw) -> aiohttp.web.Response:
         return aiohttp.web.json_response({
