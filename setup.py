5729ce35c13f741a147dcf113111965019997ffb
chris sira <cbsira@gmail.com>
2022-07-29 07:22:09 -0400

    [setup] bump to 0.3.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
         packages=find_packages(),
         package_data={"ponty": ["py.typed"]},
         name="ponty",
-        version="0.3.0",
+        version="0.3.1",
         license="BSD",
         url="https://github.com/csira/ponty",
         description="Minimal async web framework, built on aiohttp.",
