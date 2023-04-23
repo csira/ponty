340bb760869eb4108efd290faadc428c2b14cb00
chris sira <cbsira@gmail.com>
2023-04-23 16:16:32 -0400

    [setup] bump to 0.3.8

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'Ponty'
 copyright = '2023, ponty'
 author = 'ponty'
-release = '0.3.7'
+release = '0.3.8'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
